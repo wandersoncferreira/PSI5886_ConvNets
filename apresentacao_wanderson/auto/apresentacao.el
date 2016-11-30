d0c581b370c3fe25125d4baf5cd3d8df84781546
Wanderson <bartuka@ubuntu.Home>
2016-11-30 00:50:56 -0200

    apresentacao

--- apresentacao_wanderson/auto/apresentacao.el
+++ apresentacao_wanderson/auto/apresentacao.el
@@ -13,12 +13,15 @@
    (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
    (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
    (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "url")
    (TeX-run-style-hooks
     "latex2e"
     "beamer"
     "beamer11"
     "inputenc"
     "fontenc"
-    "lipsum"))
+    "lipsum"
+    "float"
+    "listings"))
  :latex)
 
